69f0b47164543c67efbadbad18c9d8324a5c3247
lukaspbeyer <lukas@lukasbeyer.com>
2022-11-28 23:01:41 +0100

    Switch to BinaryDataManager

--- nodes/MeisterTask/AttachmentDescription.ts
+++ nodes/MeisterTask/AttachmentDescription.ts
@@ -1,5 +1,6 @@
 import { INodeProperties, NodeOperationError } from 'n8n-workflow';
 import FormData from 'form-data';
+import { BinaryDataManager } from 'n8n-core';
 
 // Operations for "tasks"
 export const attachmentOperations: INodeProperties[] = [{
@@ -48,39 +49,46 @@ export const attachmentOperations: INodeProperties[] = [{
 
 						// get binary data from node input
 						const item = this.getInputData();
-						if (!item.binary) {
+						if (item.binary === undefined) {
 							throw new NodeOperationError(this.getNode(), 'No binary data exists on item!', {
 								itemIndex: this.getItemIndex(),
 							});
 						}
 
-						if (item.binary[binaryPropertyName] === undefined) {
+						if (item.binary[binaryPropertyName] === undefined || !binaryPropertyName.trim()) {
 							throw new NodeOperationError(
 								this.getNode(),
-								`No binary data property "${binaryPropertyName}" does exist on item!`,
-								{ itemIndex: this.getItemIndex() },
-							);
+								`No binary data property "${binaryPropertyName}" does exist on item!`, {
+								itemIndex: this.getItemIndex()
+							});
 						}
 
-						// get binary content and type from node input
-						const binaryData = item.binary[binaryPropertyName];
-						const content = Buffer.from(binaryData.data, 'base64');
-						const contentType = binaryData.mimeType;
+						// get binary content
+						const binaryFile = item.binary[binaryPropertyName];
+						const binaryFileType = binaryFile.mimeType;
+						const binaryFileName = binaryFile.fileName;
+						const buffer = await BinaryDataManager.getInstance().retrieveBinaryData(binaryFile);
+
+						// get file name
+						const fileNameParameter = this.getNodeParameter('name') as string | undefined;
+						if (!binaryFileName && !fileNameParameter) {
+							throw new NodeOperationError(this.getNode(), 'No file name given for attachment upload.');
+						}
 
 						// construct form body
 						const body = new FormData();
-						body.append('name', this.getNodeParameter('name') as string);
-						body.append('local', content, {
-							contentType,
-							filename: this.getNodeParameter('name') as string,
+						body.append('name', fileNameParameter || binaryFileName);
+						body.append('local', buffer, {
+							contentType: binaryFileType,
+							filename: fileNameParameter || binaryFileName,
 						});
 
 						// Set the headers
-						if (!requestOptions.headers) requestOptions.headers = {};
-						requestOptions.headers = body.getHeaders();
+						//if (!requestOptions.headers) requestOptions.headers = {};
+						//requestOptions.headers = body.getHeaders();
 
 						// Return the request data
-						requestOptions.body = body.getBuffer();
+						requestOptions.body = body;
 						return requestOptions;
 					},
 				],
